74bfebd8778ba1d9bd05ef9288360d92cdecd3ca
Maczaj <daark.lith@gmail.com>
2014-01-08 14:57:14 +0100

    Zbior podstawowych testow gotowy, testy fs_stat olalem, bo tak sie mnie zdaje ze nie ma co w tej funkcyji testowac, toc po prostu przeslanie tego co OS zwroci

--- fs_client.cpp
+++ fs_client.cpp
@@ -6,6 +6,8 @@
 #define MAX_SIZE 4096
 #define NO_SUCH_FILE_ERROR -200999
 #define NO_LOCK_ERROR -200998
+#define LOCKING_NOT_POSSIBLE_ERROR -200997
+
 #define DATA_FOR_WRITING "some test data for writing and reading"
 #define FILE_NAME "test_file"
 
@@ -19,6 +21,8 @@ int srvhndl;
 void test1();
 void test2();
 void test3();
+void test4();
+void test5();
 
 //=================== MAIN ========================//
 int main(int argc,char *argv[]){
@@ -29,10 +33,8 @@ int main(int argc,char *argv[]){
 	test1();
 	test2();
 	test3();
-	.
-	.
-	.
-	testn();
+	test4();
+	test5();
 	
 	
 	fs_close_server(srvhndl);
@@ -93,10 +95,9 @@ void test2(){
 	test fs_read
 */
 void test3(){
-	cout << "Test case 3: reading from non-existing file, opening file for reading without locking and reading from file" << endl;
+	cout << "Test case 3: reading from non-existing file, opening file for reading without locking and reading from file...";
 	
-	//4 kB na bufor, i tak az za, ale....
-	char buffer[4096];
+	char buffer[200];
 	
 	//najpierw czytanie z nieistniejacego
 	int result = fs_read ( srvhndl , 999999999 , (void *) buffer , strlen(DATA_FOR_WRITING) ):
@@ -116,5 +117,64 @@ void test3(){
 	
 	fs_close(srvhndl , fd) ;
 	cout << "OK" << endl;
+}
+
+/**
+	test fs_lseek
+*/
+void test4(){
+	cout << "Test case 4: Opening file, reading first characters, moving back to beginning and reading again...";
+	char buffer[50];
+	char secon_buffer[50];
+	
+	int fd = fs_open (srvhndl, FILE_NAME, O_READ );
+	assert (fd > 0);
+	int result = fs_lock( srvhndl, fd , READ );
+	assert (result == 0) ;
+	
+	int read_amount = 5;
+	result = fs_read ( srvhndl , fd , (void *) buffer , read_amount );
+	assert ( result == read_amount ) ;
+	
+	result = fs_lseek ( srvhndl , fd , -read_amount , SEEK_CUR ) ;
+	assert (result == 0 );
+	result = fs_read ( srvhndl , fd , (void *) second_buffer , read_amount );
+	assert ( result == 0 );
+	assert ( !strcmp ( buffer, second_buffer ) );
+	
+	fs_close (srvhndl, fd) ;
+	cout << "OK" << endl;
+}
+
+/**
+	test fs_lock
+*/
+void test5(){
+	cout << "Test case 5a: Opening file, locking for reading , locking for writing, closing...";
+	
+	int fd = fs_open (srvhndl, FILE_NAME, O_READ );
+	assert ( fd > 0 );
+	
+	int result = fs_lock( srvhndl, fd , READ );
+	assert (result == 0 ) ;
+	result = fs_lock ( srvhndl , fd , WRITE );
+	assert ( result == LOCKING_NOT_POSSIBLE_ERROR );
+
+	fs_close( srvhndl, fd) ;
+	cout << "OK" << endl;
+	
+	cout << "Test case 5b: Opening file, locking for writing, locking for reading, closing...";
+	
+	fd = fs_open (srvhndl, FILE_NAME, O_READ );
+	assert ( fd > 0 ) ;
+	result = fs_lock ( srvhndl , fd , WRITE );
+	assert ( result == 0 ) ;
+	
+	result = fs_lock ( srvhndl , fd , READ );
+	assert ( result == LOCKING_NOT_POSSIBLE_ERROR );
+	
+	fs_close( srvhndl , fd );
+	
+	cout << "OK";
 }
 
